a307db469a57247c07bc8fb72984011f5711f338
sarathc <sarathc@airtelbank.com>
2021-07-15 13:26:33 +0530

    Added a HelloWorldController Without test.

---
+++ helloworld/src/main/java/org/sarathcall/helloworld/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package org.sarathcall.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+     @RequestMapping("/") 
+     public String hello () {
+         return "Hello World";
+     }
+}

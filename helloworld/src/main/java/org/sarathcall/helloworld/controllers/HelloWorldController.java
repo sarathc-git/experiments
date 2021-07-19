3bce868af61459370aa596d01a08262ef838f216
sarathc <sarathc@airtelbank.com>
2021-07-19 14:34:08 +0530

    Added version to the apis (v0.2)

--- helloworld/src/main/java/org/sarathcall/helloworld/controllers/HelloWorldController.java
+++ helloworld/src/main/java/org/sarathcall/helloworld/controllers/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-     @RequestMapping("/") 
+     @RequestMapping("/v0.2/") 
      public String hello () {
          return "Hello World";
      }

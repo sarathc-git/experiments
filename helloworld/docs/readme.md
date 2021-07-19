ff797dcee404316c340515a3c5d350c89e685344
sarathc <sarathc@airtelbank.com>
2021-07-19 12:31:33 +0530

    Added test for the controller as e2e test.

---
+++ helloworld/docs/readme.md
@@ -0,0 +1,39 @@
+# Spring boot application best practices
+
+## Introduction
+
+    This is a sample project starting from a simple hello world and to be iteratively expanded to deliver multiple capabilities using industry best practices.
+
+## Stages
+
+### HelloWorld Spring boot Application
+
+### HelloWorld Application with Controller
+
+#### Without test cases
+
+#### With test cases
+
+### Application Testing with K6
+
+#### Using XXXX for creating test data
+
+### Application with Prometheus
+
+### Settig up observability dashboard
+
+### Application API documentation with swagger
+
+### Application Deployment on Docker
+
+### Application deployment using buildpacks.io 
+
+### Application deployment on Kubernetes
+
+### Application with controller taking an input (name)
+
+### Application with controller validations
+
+### Application with multiple form inputs
+
+### Application with multipart form submission
---
+++ helloworld/src/test/java/org/sarathcall/helloworld/controllers/HelloWorldControllerTest.java
@@ -0,0 +1,29 @@
+package org.sarathcall.helloworld.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloWorldControllerTest {
+
+    @LocalServerPort 
+    private int localPort; 
+    
+    @Autowired
+    private TestRestTemplate client;
+     
+    @Test
+    @Tag("e2e")
+    public void testShouldReturnMessage() {
+        String response = client.getForObject("http://localhost:" + localPort + "/", String.class);
+        assertEquals("Hello World", response);
+    }
+
+    
+}
